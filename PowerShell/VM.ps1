49ef6d2a5c288575d962888feb9821d91c0a8468
Ron Kaiser <ron.kaiser@shopic.co>
2021-09-15 20:34:53 +0300

    Add create vm file

---
+++ PowerShell/VM.ps1
@@ -0,0 +1,11 @@
+
+
+# Create virtual machine with all parameters
+new-azvm
+  -ResourceGroupName 'RESOURCE_GROUP_NAME' `
+  -Name 'TestVM' `
+  -VirtualNetworkName 'MyVNet' `
+  -SubnetName 'MySubnet' `
+  -SecurityGroupName 'MySecurityGroup' `
+  -PublicIpAddressName 'myPubIP' `
+  -OpenPorts 80, 3389
